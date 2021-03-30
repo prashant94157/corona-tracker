21d2a6b982b43be7eca6d95c6bf964353c3c7731
prashant94157 <0166621m@gmail.com>
2021-03-30 22:34:54 +0530

    Updates

---
+++ precache-manifest.98991b01b8350a31d51cc82754699fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03ce282e4b76bc72e41ea4dc87a15c0",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "6e52ba54827eef18ab5b",
+    "url": "/corona-tracker/static/css/main.0dae7174.chunk.css"
+  },
+  {
+    "revision": "d31022c853da34026487",
+    "url": "/corona-tracker/static/js/2.cb98e94a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/corona-tracker/static/js/2.cb98e94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e52ba54827eef18ab5b",
+    "url": "/corona-tracker/static/js/main.96ccbe23.chunk.js"
+  },
+  {
+    "revision": "bf17874e7839ba08222a",
+    "url": "/corona-tracker/static/js/runtime-main.87fc3e90.js"
+  },
+  {
+    "revision": "1bdacb4d02b85c862a176655468a5f33",
+    "url": "/corona-tracker/static/media/OIP.1bdacb4d.jpg"
+  }
+]);
